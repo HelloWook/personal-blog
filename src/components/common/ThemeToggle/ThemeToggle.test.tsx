728789f971b060df5a241ee7b429abc2ba08d719
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-06-24 22:37:40 +0900

    ✨ Feat: themeToggle 컴포넌트 구현

---
+++ src/components/common/ThemeToggle/ThemeToggle.test.tsx
@@ -0,0 +1,29 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import ThemeToggle from './ThemeToggle';
+
+describe('ThemeToggle', () => {
+  it('컴포넌트가 정상적으로 렌더링된다', () => {
+    render(<ThemeToggle />);
+    const checkbox = screen.getByRole('checkbox');
+    expect(checkbox).toBeInTheDocument();
+  });
+
+  it('초기 테마는 synthwave이다', async () => {
+    render(<ThemeToggle />);
+
+    const htmlElement = document.documentElement;
+    const theme = htmlElement.getAttribute('data-theme');
+    expect(theme).toBe('pastel');
+  });
+});
+
+it('테마를 변경하면 data-theme 속성이 업데이트된다', () => {
+  render(<ThemeToggle />);
+
+  const checkbox = screen.getByRole('checkbox');
+  fireEvent.click(checkbox);
+
+  const htmlElement = document.documentElement;
+  const theme = htmlElement.getAttribute('data-theme');
+  expect(theme).toBe('synthwave');
+});
