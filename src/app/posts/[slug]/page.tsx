985f3f49d49ff3d89b8387d1c124f6c300555c3d
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-07-17 02:37:49 +0900

    💊 Fix: next 15에 맞춰 params 비동기로 받아도록 수정

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,11 +1,12 @@
 import { MDXRemote } from 'next-mdx-remote/rsc';
 
 import parseMdx from '@/util/parseMDX';
+import { use } from 'react';
 
-export default async function PostPage({ params }: { params: { slug: string } }) {
-  const { mdxContent, data } = parseMdx(params.slug);
-  console.log('Post data:', data);
-  console.log('MDX Content:', mdxContent);
+export default async function PostPage({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = use(params);
+  const { mdxContent } = parseMdx(slug);
+
   return (
     <article>
       <MDXRemote source={mdxContent} />
