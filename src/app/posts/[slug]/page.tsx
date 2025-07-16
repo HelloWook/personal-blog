3b724a1a81ae5554f5641c24227a94853ba5a69e
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-07-17 02:38:56 +0900

    💊 Fix: use async 제거

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -3,7 +3,7 @@ import { MDXRemote } from 'next-mdx-remote/rsc';
 import parseMdx from '@/util/parseMDX';
 import { use } from 'react';
 
-export default async function PostPage({ params }: { params: Promise<{ slug: string }> }) {
+export default function PostPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = use(params);
   const { mdxContent } = parseMdx(slug);
 
