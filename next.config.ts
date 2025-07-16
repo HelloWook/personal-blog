9ee0d13f2fc9fdd49061a2b66308e516f70fff0c
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-07-17 01:25:16 +0900

    🌞 env: 필요 의존성 설치
    mdx parser 및 loader

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
 import type { NextConfig } from 'next';
+import createMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   eslint: {
     dirs: ['src'],
   },
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/,
+});
+
+export default withMDX(nextConfig);
