5884431176aa887bfeb398d160317c34f8706c8c
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-08-16 03:55:39 +0900

    ✨ Feat: 게시글 상세 페이지 ssg 적용

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import PostCard from '@/components/Post/PostCard/PostCard';
+import PostSeriesList from '@/components/Post/PostSeriesList/PostSeriesList';
+import { getSeries, getPosts } from '@/util/file';
+import Albam from '@/asset/알밤.png';
+interface PostPageProps {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
 
-const Post = () => {
-  return <div>개발 중..</div>;
+const PostPage = async ({ searchParams }: PostPageProps) => {
+  const seriesList = await getSeries('contents/posts');
+
+  const series = (await searchParams).series || 'All';
+
+  const posts = getPosts('contents/posts').filter((post) => {
+    if (series === 'All') return true;
+    else return post.series === series;
+  });
+
+  const title = series ? `${series} Posts` : 'All Posts';
+
+  return (
+    <div className='w-full'>
+      <h2 className='mb-2 text-3xl text-center'>{title}</h2>
+      <p className='mb-6 text-xl text-center text-gray-300'>{posts.length} Posts</p>
+      <PostSeriesList seriesList={seriesList} />
+      <div className='grid grid-cols-2 gap-8'>
+        {posts.map((post, idx) => {
+          const { excerpt, fileName, title } = post;
+          return <PostCard key={idx} excerpt={excerpt} fileName={fileName} thumbnail={Albam} title={title} />;
+        })}
+      </div>
+    </div>
+  );
 };
 
-export default Post;
+export default PostPage;
