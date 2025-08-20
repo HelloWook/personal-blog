23d1d1571bedd3b1f3b5bdb89d3db07a403a23f0
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-08-20 17:09:47 +0900

    ✨ Feat: 코드 하이라이팅

--- src/app/posts/[fileName]/page.tsx
+++ src/app/posts/[fileName]/page.tsx
@@ -1,8 +1,10 @@
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import { getFileNames } from '@/util/file';
+import rehypePrettyCode, { type Options } from 'rehype-pretty-code';
 
 import parseMdx from '@/util/parseMDX';
 import { use } from 'react';
+import Pre from '@/components/Pre/Pre';
 
 interface PostDetailPageProps {
   params: Promise<{ fileName: string }>;
@@ -11,6 +13,13 @@ export async function generateStaticParams() {
   const fileNames = await getFileNames('contents/posts');
   return fileNames.map((fileName) => ({ fileName }));
 }
+const prettyCodeOptions: Options = {
+  theme: {
+    light: 'nord',
+    dark: 'night-owl',
+  },
+  transformers: [],
+};
 
 export default function PostDetailPage({ params }: PostDetailPageProps) {
   const { fileName } = use(params);
@@ -18,7 +27,17 @@ export default function PostDetailPage({ params }: PostDetailPageProps) {
 
   return (
     <article>
-      <MDXRemote source={mdxContent} />
+      <MDXRemote
+        source={mdxContent}
+        options={{
+          mdxOptions: {
+            rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
+          },
+        }}
+        components={{
+          pre: Pre,
+        }}
+      />
     </article>
   );
 }
