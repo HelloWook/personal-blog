c87857dd5d53aa7603252e2964189882356a1a87
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-08-20 19:36:04 +0900

    ✨ Feat: 프로젝트  페이지 서브 타이틀 추가

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,13 @@
 import ProjectList from '@/components/Project/ProjectList/ProjectList';
+import SubTitle from '@/components/SubTitle/SubTitle';
 import React from 'react';
 
 const Post = () => {
   return (
-    <div className=''>
+    <>
+      <SubTitle title='프로젝트 목록' description='참여한 프로젝트 목록입니다.' />
       <ProjectList />
-    </div>
+    </>
   );
 };
 
