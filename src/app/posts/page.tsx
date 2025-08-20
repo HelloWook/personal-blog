c17d7066f2ef23ebeb0675715a7a9b20c2bc6953
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-08-20 23:15:30 +0900

    🚚 Chore: postpage 정적으로 불러오도록 설정

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,4 +1,6 @@
 import PostSeries from '@/components/Post/PostSeries/PostSeries';
+export const dynamic = 'force-static';
+import { getSeries, getPosts } from '@/util/file';
 
 type SearchParams = Promise<Record<string, string | string[] | undefined>>;
 
@@ -8,12 +10,14 @@ interface PostPageProps {
 
 const PostPage = async ({ searchParams }: PostPageProps) => {
   const sp = await searchParams;
-  console.log(sp);
 
   const raw = sp.series ? sp.series : undefined;
   const series = Array.isArray(raw) ? raw[0] : raw ?? 'All';
 
-  return <div className='w-full'>{<PostSeries series={series} />}</div>;
+  const seriesList = getSeries('contents/posts');
+  const postList = getPosts('contents/posts');
+
+  return <div className='w-full'>{<PostSeries series={series} postList={postList} seriesList={seriesList} />}</div>;
 };
 
 export default PostPage;
--- src/components/Post/PostSeries/PostSeries.tsx
+++ src/components/Post/PostSeries/PostSeries.tsx
@@ -1,26 +1,26 @@
-import { getSeries } from '@/util/file';
+import PostSeriesList from '@/components/Post/PostSeriesList/PostSeriesList';
+import { Post } from '@/types/post';
 
 interface PostSeriesProps {
   series: string;
+  postList: Post[];
+  seriesList: string[];
 }
 
-const PostSeries = ({ series }: PostSeriesProps) => {
-  const seriesList = getSeries('contents/posts');
-
-  console.log(seriesList);
-  // const posts = getPosts('contents/posts').filter((post) => {
-  //   if (series === 'All') return true;
-  //   else return post.series === series;
-  // });
+const PostSeries = ({ series, postList, seriesList }: PostSeriesProps) => {
+  const posts = postList.filter((post) => {
+    if (series === 'All') return true;
+    else return post.series === series;
+  });
 
   const title = series ? `${series} Posts` : 'All Posts';
 
   return (
     <>
       <h2 className='mb-2 text-3xl text-center'>{title}</h2>
-      {/* <p className='mb-6 text-xl text-center text-gray-300'>{seriesList.length} Posts</p>
-      <PostSeriesList seriesList={seriesList} /> */}
-      <div className='grid grid-cols-1 gap-8 md:grid-cols-2' />
+      <p className='mb-6 text-xl text-center text-gray-300'>{posts.length} Posts</p>
+      <PostSeriesList seriesList={seriesList} />
+      <div className='grid grid-cols-1 gap-8 md:grid-cols-2'></div>
     </>
   );
 };
