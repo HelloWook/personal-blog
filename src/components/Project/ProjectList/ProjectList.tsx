9679cf6004b0254e1f5eb77699b1d5b7b0755767
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-08-20 19:22:47 +0900

    ✨ Feat: 프로젝트 페이지 개발

---
+++ src/components/Project/ProjectList/ProjectList.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { projects } from '@/Data/project';
+import ProjectCard from '../ProjectCard/ProjectCard';
+
+const ProjectList = () => {
+  return (
+    <div className='grid justify-between grid-cols-1 gap-4 sm:grid-cols-2'>
+      {projects.map((project, index) => (
+        <ProjectCard key={index} {...project} />
+      ))}
+    </div>
+  );
+};
+
+export default ProjectList;
