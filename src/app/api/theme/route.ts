15c2efcf4bf49f37133c980c7fb4ebceee687737
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-09-05 13:42:07 +0900

    🚚 Chore: 테마 설정 서버 방어 코드 작성

--- src/app/api/theme/route.ts
+++ src/app/api/theme/route.ts
@@ -4,13 +4,15 @@ import { themeManager } from '@/util/tokenManger';
 export async function POST(request: NextRequest) {
   try {
     const res = await request.json();
+
     const { setTheme } = await themeManager();
 
+    console.log('Received theme:', res.theme);
     await setTheme(res.theme);
 
     return NextResponse.json({ message: 'ok' });
   } catch (error) {
-    console.error('Error in theme API:', error);
-    return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    console.error('에러 발생 :', error);
+    return NextResponse.json({ error: '서버 에러 발생' }, { status: 500 });
   }
 }
