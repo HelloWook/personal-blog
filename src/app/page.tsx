be40d06e7710283cbfb9e9d5eda02c282660e996
남정욱 <113816822+HelloWook@users.noreply.github.com>
2025-10-22 17:11:25 +0900

    📝 Docs: 새 블로그 글 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,16 +11,19 @@ export default async function Home() {
       <Introduce />
       <SubTitle title='최근 포스트' description='개발과 관련된 다양한 주제들을 다룹니다' />
       <div className='flex flex-col gap-6'>
-        {posts.slice(0, 4).map((post, idx) => (
-          <PostCard
-            key={idx}
-            fileName={post.fileName}
-            title={post.title}
-            excerpt={post.excerpt}
-            thumbnail={post.thumbnail}
-            blurDataURL={post.blurDataURL}
-          />
-        ))}
+        {posts
+          .slice(0, 4)
+          .reverse()
+          .map((post, idx) => (
+            <PostCard
+              key={idx}
+              fileName={post.fileName}
+              title={post.title}
+              excerpt={post.excerpt}
+              thumbnail={post.thumbnail}
+              blurDataURL={post.blurDataURL}
+            />
+          ))}
       </div>
     </>
   );
